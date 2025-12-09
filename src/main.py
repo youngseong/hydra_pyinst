71b2f88ce96ef6297f83b4917a39b6b2eb18821c
youngseong <youngseong0601@gmail.com>
2025-12-09 22:37:14 +0900

    format main.py

--- src/main.py
+++ src/main.py
@@ -7,7 +7,7 @@ from omegaconf import DictConfig
 
 
 def get_config_path() -> str:
-    frozen = getattr(sys, 'frozen', False) and hasattr(sys, '_MEIPASS')
+    frozen = getattr(sys, "frozen", False) and hasattr(sys, "_MEIPASS")
     bin_path = Path(sys.executable if frozen else __file__)
     return str(bin_path.parent.parent)
 
@@ -15,6 +15,7 @@ def get_config_path() -> str:
 @hydra.main(version_base=None, config_path=get_config_path(), config_name="config")
 def main(config: DictConfig):
     print(config.db)
+
 
 if __name__ == "__main__":
     main()
