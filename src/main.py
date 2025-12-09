bff66f5f5ab7d3f331a526342fbb7cd31c3f8e43
youngseong <youngseong0601@gmail.com>
2025-12-09 21:43:46 +0900

    determine config path based on frozen status

--- src/main.py
+++ src/main.py
@@ -1,9 +1,18 @@
 #!/usr/bin/env python
 
+from pathlib import Path
+import sys
 import hydra
 from omegaconf import DictConfig
 
-@hydra.main(version_base=None, config_path="..", config_name="config")
+
+def get_config_path() -> str:
+    frozen = getattr(sys, 'frozen', False) and hasattr(sys, '_MEIPASS')
+    bin_path = Path(sys.executable if frozen else __file__)
+    return str(bin_path.parent.parent)
+
+
+@hydra.main(version_base=None, config_path=get_config_path(), config_name="config")
 def main(config: DictConfig):
     print(config.db)
 
